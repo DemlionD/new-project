f1332ce2dea59ad36beff1e727e0e4b4d9af4447
DemlionD <Demlion24@gmail.com>
2023-07-16 01:01:02 +0600

    third

--- test.js
+++ test.js
@@ -1,6 +1,16 @@
 "use strict";
 
-let numberOfFilms = prompt(`Сколько фильмов вы посмотрели?`, ``);
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt(`Сколько фильмов вы посмотрели?`, ``);
+    
+    while (numberOfFilms == `` || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt(`Сколько фильмов вы посмотрели?`, ``);
+    }
+} 
+
+start();
 
 let personalMovieDB = {
     count: numberOfFilms,
@@ -10,28 +20,53 @@ let personalMovieDB = {
     privat: false
 };
 
-for (let i = 0; i < 2; i++) {
-    let a = prompt(`Один из последних просмотренных фильмов?`, ``),
-        b = prompt(`На сколько оцените его?`, ``);
-
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-        console.log(`Done`);
-    } else {
-        console.log(`Error`);
-        i--;
-    }
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        let a = prompt(`Один из последних просмотренных фильмов?`, ``),
+            b = prompt(`На сколько оцените его?`, ``);
     
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log(`Done`);
+        } else {
+            console.log(`Error`);
+            i--;
+        }
+        
+    }
 }
 
-if (personalMovieDB.count < 10) {
-    alert(`Просмотрено довольно мало фильмов!`);
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    alert(`Вы классический зритель!`);
-} else if (personalMovieDB.count >= 30) {
-    alert(`Вы киноман!`);
-} else {
-    alert(`Произошла ошибка`);
+rememberMyFilms();
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        alert(`Просмотрено довольно мало фильмов!`);
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        alert(`Вы классический зритель!`);
+    } else if (personalMovieDB.count >= 30) {
+        alert(`Вы киноман!`);
+    } else {
+        alert(`Произошла ошибка`);
+    }
 }
 
-console.log(personalMovieDB);
+detectPersonalLevel();
+
+function showMyDB() {
+    if (personalMovieDB.privat == false) {
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB();
+
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++) {
+        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ` + i);
+    }
+}
+
+writeYourGenres();
+
+
+// вопросы: он в приват функции написал хидден и !хидден я же сделал через иф но работает одинаково, еще он поставил после жанра ${i} а я поставил + i и разницы в итоге тоже, по крайней мере я ее не заметил, так вот если у меня работает значит у меня все правильно и можно сделать так как сделал я или нужно делать так как сделал он ведь он знает лучше
